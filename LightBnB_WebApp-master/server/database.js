1a269f7852de320e64fa0ded15c69c3004b8c71f
Rajaa Omary <omary.rajaa@gmail.com>
2020-11-26 18:42:52 +0000

    Implement Users functions

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -18,17 +18,23 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
-}
+  const sqlQuery = {
+    text: `SELECT * 
+    FROM users 
+    WHERE email = $1;`,
+    values: [email]
+  };
+  return pool
+    .query(sqlQuery)
+    .then((result) => {
+      if (result.rowCount > 0) {
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((error) => error);
+};
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -37,8 +43,17 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
-}
+  const sqlQuery = {
+    text: `SELECT * 
+    FROM users 
+    WHERE id = $1;`,
+    values: [id]
+  };
+  return pool
+    .query(sqlQuery)
+    .then((result) => result.rows[0])
+    .catch((error) => error);
+};
 exports.getUserWithId = getUserWithId;
 
 
@@ -48,11 +63,21 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
+  const sqlQuery = {
+    text: `INSERT INTO users (name, password, email) VALUES ($1, $2, $3) RETURNING *;`,
+    values: [user.name, user.password, user.email],
+  };
+  return pool
+    .query(sqlQuery)
+    .then((result) => result.rows[0])
+    .catch((error) => error);
+  /*
   const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
   return Promise.resolve(user);
-}
+  */
+};
 exports.addUser = addUser;
 
 /// Reservations
@@ -64,7 +89,7 @@ exports.addUser = addUser;
  */
 const getAllReservations = function(guest_id, limit = 10) {
   return getAllProperties(null, 2);
-}
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
@@ -80,8 +105,8 @@ const getAllProperties = function(options, limit = 10) {
   SELECT * FROM properties
   LIMIT $1
   `, [limit])
-  .then(res => res.rows);
-}
+    .then(res => res.rows);
+};
   
 
 exports.getAllProperties = getAllProperties;
@@ -97,5 +122,5 @@ const addProperty = function(property) {
   property.id = propertyId;
   properties[propertyId] = property;
   return Promise.resolve(property);
-}
+};
 exports.addProperty = addProperty;
